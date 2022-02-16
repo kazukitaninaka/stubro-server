9938538fca408b9e4d34b0d52cb0cc70f6e54c60
kazukitaninaka <onelive555@gmail.com>
2022-02-16 16:39:16 +0900

    create auth middleware

--- middleware/auth.go
+++ middleware/auth.go
@@ -3,11 +3,18 @@ package middleware
 import (
 	"log"
 	"net/http"
+
+	"github.com/kazukitaninaka/stubro-server/config"
 )
 
 func AuthMiddleware(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println("middleware", r.Header)
+		idToken := r.Header["Authorization"][0]
+		token, err := config.AuthClient.VerifyIDToken(config.Ctx, idToken)
+		if err != nil {
+			log.Fatalf("error verifying ID token: %v\n", err)
+		}
+		log.Printf("Verified ID token: %v\n", token)
 		h.ServeHTTP(w, r)
 	})
 }
