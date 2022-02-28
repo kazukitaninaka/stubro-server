521ec0136578ee0906ea079aeffdbcaa10ee54a4
kazukitaninaka <onelive555@gmail.com>
2022-02-28 18:06:14 +0900

    move: firebase_credentialsをアプリケーションフォルダ内に移動

--- config/firebase.go
+++ config/firebase.go
@@ -13,7 +13,7 @@ var AuthClient *auth.Client
 var Ctx = context.Background()
 
 func InitFirebase() {
-	opt := option.WithCredentialsFile("/Users/kazuki.taninaka/Downloads/stubro-7d23a-firebase-adminsdk-gx4p1-67f9382c79.json")
+	opt := option.WithCredentialsFile("firebase_credentials.json")
 	app, err := firebase.NewApp(Ctx, nil, opt)
 	if err != nil {
 		log.Fatalf("error initializing app: %v\n", err)
