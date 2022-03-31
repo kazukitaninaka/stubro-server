a542482e894c406d8495a57a554fc6e7786ed44f
kazukitaninaka <onelive555@gmail.com>
2022-03-31 10:27:42 +0900

    update: read mysql on docker

--- openapi/db.go
+++ openapi/db.go
@@ -3,6 +3,7 @@ package openapi
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -25,8 +26,9 @@ var Db *gorm.DB
 
 func InitDb() {
 	var err error
-	dsn := "kazukitaninaka:db-password@/test_db?charset=utf8&parseTime=True"
-	Db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	DSN := os.Getenv("DSN")
+	fmt.Println(DSN)
+	Db, err = gorm.Open(mysql.Open(DSN), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Database connection failed: ", err)
 	} else {
@@ -39,7 +41,7 @@ func InitDb() {
 	// defer db.Close()
 
 	// Auto Migration
-	// Db.AutoMigrate(&Consultation{})
+	// Db.AutoMigrate(&Consultation{}, &Mentor{}, &Term{})
 
 	// err = Db.SetupJoinTable(&Mentor{}, "Types", &TypeMentor{})
 	// if err != nil {
@@ -49,85 +51,85 @@ func InitDb() {
 }
 
 func seed(d *gorm.DB) {
-	// terms := []Term{
-	// 	{Name: "〜1週間"},
-	// 	{Name: "1週間〜1ヶ月"},
-	// 	{Name: "1ヶ月〜3ヶ月"},
-	// 	{Name: "3ヶ月〜半年"},
-	// 	{Name: "半年〜1年"},
-	// 	{Name: "2年"},
-	// 	{Name: "3年"},
-	// 	{Name: "4年"},
-	// }
+	terms := []Term{
+		{Name: "〜1週間"},
+		{Name: "1週間〜1ヶ月"},
+		{Name: "1ヶ月〜3ヶ月"},
+		{Name: "3ヶ月〜半年"},
+		{Name: "半年〜1年"},
+		{Name: "2年"},
+		{Name: "3年"},
+		{Name: "4年"},
+	}
 
-	// for _, t := range terms {
-	// 	d.Create(&t)
-	// }
-	// mentors := []Mentor{
-	// 	{
-	// 		Username:    "Kazuki",
-	// 		Email:       "kazuki@gmail.com",
-	// 		Image:       "https://placehold.jp/150x150.png",
-	// 		ClickCount:  0,
-	// 		Destination: "アメリカ",
-	// 		EduOrg:      "Reading High School",
-	// 		Term:        Term{ID: 5, Name: "半年〜1年"},
-	// 		Types: []Type{
-	// 			{ID: 7, Name: "大学留学"},
-	// 			{ID: 10, Name: "私費留学"},
-	// 			{ID: 9, Name: "寮"},
-	// 		},
-	// 		Price: 100,
-	// 	},
-	// 	{
-	// 		Username:    "Eren",
-	// 		Email:       "eren@gmail.com",
-	// 		Image:       "https://placehold.jp/150x150.png",
-	// 		ClickCount:  0,
-	// 		Destination: "イギリス",
-	// 		EduOrg:      "University of Oxford",
-	// 		Term:        Term{ID: 8, Name: "4年"},
-	// 		Types: []Type{
-	// 			{ID: 7, Name: "大学留学"},
-	// 			{ID: 10, Name: "私費留学"},
-	// 			{ID: 9, Name: "寮"},
-	// 		},
-	// 		Price: 3000,
-	// 	},
-	// 	{
-	// 		Username:    "Armin",
-	// 		Email:       "armin@gmail.com",
-	// 		Image:       "https://placehold.jp/150x150.png",
-	// 		ClickCount:  0,
-	// 		Destination: "デンマーク",
-	// 		EduOrg:      "University of Copenhagen",
-	// 		Term:        Term{ID: 5, Name: "半年〜1年"},
-	// 		Types: []Type{
-	// 			{ID: 7, Name: "大学留学"},
-	// 			{ID: 6, Name: "交換留学"},
-	// 		},
-	// 		Price: 2500,
-	// 	},
-	// 	{
-	// 		Username:    "Mikasa",
-	// 		Email:       "mikasa@gmail.com",
-	// 		Image:       "https://placehold.jp/150x150.png",
-	// 		ClickCount:  0,
-	// 		Destination: "アメリカ",
-	// 		EduOrg:      "University of California, Los Angeles",
-	// 		Term:        Term{ID: 5, Name: "半年〜1年"},
-	// 		Types: []Type{
-	// 			{ID: 7, Name: "大学留学"},
-	// 			{ID: 6, Name: "交換留学"},
-	// 			{ID: 9, Name: "寮"},
-	// 			{ID: 8, Name: "奨学金利用"},
-	// 		},
-	// 		Price: 2000,
-	// 	},
-	// }
+	for _, t := range terms {
+		d.Create(&t)
+	}
+	mentors := []Mentor{
+		{
+			Username:    "Kazuki",
+			Email:       "kazuki@gmail.com",
+			Image:       "https://placehold.jp/150x150.png",
+			ClickCount:  0,
+			Destination: "アメリカ",
+			EduOrg:      "Reading High School",
+			Term:        Term{ID: 5, Name: "半年〜1年"},
+			Types: []Type{
+				{ID: 7, Name: "大学留学"},
+				{ID: 10, Name: "私費留学"},
+				{ID: 9, Name: "寮"},
+			},
+			Price: 100,
+		},
+		{
+			Username:    "Eren",
+			Email:       "eren@gmail.com",
+			Image:       "https://placehold.jp/150x150.png",
+			ClickCount:  0,
+			Destination: "イギリス",
+			EduOrg:      "University of Oxford",
+			Term:        Term{ID: 8, Name: "4年"},
+			Types: []Type{
+				{ID: 7, Name: "大学留学"},
+				{ID: 10, Name: "私費留学"},
+				{ID: 9, Name: "寮"},
+			},
+			Price: 3000,
+		},
+		{
+			Username:    "Armin",
+			Email:       "armin@gmail.com",
+			Image:       "https://placehold.jp/150x150.png",
+			ClickCount:  0,
+			Destination: "デンマーク",
+			EduOrg:      "University of Copenhagen",
+			Term:        Term{ID: 5, Name: "半年〜1年"},
+			Types: []Type{
+				{ID: 7, Name: "大学留学"},
+				{ID: 6, Name: "交換留学"},
+			},
+			Price: 2500,
+		},
+		{
+			Username:    "Mikasa",
+			Email:       "mikasa@gmail.com",
+			Image:       "https://placehold.jp/150x150.png",
+			ClickCount:  0,
+			Destination: "アメリカ",
+			EduOrg:      "University of California, Los Angeles",
+			Term:        Term{ID: 5, Name: "半年〜1年"},
+			Types: []Type{
+				{ID: 7, Name: "大学留学"},
+				{ID: 6, Name: "交換留学"},
+				{ID: 9, Name: "寮"},
+				{ID: 8, Name: "奨学金利用"},
+			},
+			Price: 2000,
+		},
+	}
 
-	// for _, m := range mentors {
-	// 	d.Create(&m)
-	// }
+	for _, m := range mentors {
+		d.Create(&m)
+	}
 
 }
